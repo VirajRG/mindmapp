b4d957eda97ed59bf00ab001f6b9baec26dbe40e
VirajRG <viraj.gawde123@gmail.com>
2019-06-15 17:36:35 +0530

    Updates

---
+++ precache-manifest.83e5e8359b3e5cc45ccdaecaf5c64a97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c8ea8f564d8f1a75c07",
+    "url": "/mindmapp/static/js/runtime~main.7c8ea8f5.js"
+  },
+  {
+    "revision": "4ea58a9c93c82fdf8155",
+    "url": "/mindmapp/static/js/main.4ea58a9c.chunk.js"
+  },
+  {
+    "revision": "9f37ec42bddb40baccc8",
+    "url": "/mindmapp/static/js/2.9f37ec42.chunk.js"
+  },
+  {
+    "revision": "4ea58a9c93c82fdf8155",
+    "url": "/mindmapp/static/css/main.d9b012a9.chunk.css"
+  },
+  {
+    "revision": "9f37ec42bddb40baccc8",
+    "url": "/mindmapp/static/css/2.6a53435e.chunk.css"
+  },
+  {
+    "revision": "c5d4a5a3a42d68a18550273db5dcb115",
+    "url": "/mindmapp/index.html"
+  }
+];
